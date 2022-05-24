da4ba7f1f70922d9e3f105089dfe4545cf1a09a6
Bart <ioovmian@naver.com>
2022-05-24 23:35:10 +0900

    Signal R 기능 적용중

--- Lecture/Hubs/ChatHub.cs
+++ Lecture/Hubs/ChatHub.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Lecture.Hubs
 {
+    [Authorize]
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+            if (message == "exit")
+            {
+                // Clients.All    : 모두에게 전달
+                // Clients.Caller : 요청자에게 전달
+                await Clients.Caller.SendAsync("ReceiveMessage", "SYSTEM", "접속이 종료되었습니다.");
+                Context.Abort();
+            }
+            else
+            {
+                //await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+                await Clients.All.SendAsync("ReceiveMessage", this.Context.User.Identity.Name, message);
+            }
         }
     }
 }
