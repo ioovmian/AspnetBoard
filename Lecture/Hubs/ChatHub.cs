5789ce79a3b250ba4fe3fc573f5c8adf2719a43b
krkwon <ioovmian@lsinvest.co.kr>
2022-05-30 17:17:00 +0900

    특정 상대에게만 메시지 보내기

--- Lecture/Hubs/ChatHub.cs
+++ Lecture/Hubs/ChatHub.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Lecture.Hubs
 {
-    [Authorize]
+    //[Authorize]
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, string targetId)
         {
 
             if (message == "exit")
@@ -18,10 +18,33 @@ namespace Lecture.Hubs
             }
             else
             {
-                //await Clients.All.SendAsync("ReceiveMessage", user, message);
+                if(string.IsNullOrWhiteSpace(targetId))
+                {
+                    await Clients.All.SendAsync("ReceiveMessage", this.Context.ConnectionId, message);
+                }
+                else
+                {
+                    //await Clients.Client(targetId.Trim()).SendAsync("ReceiveMessage", this.Context.User.Identity.Name, message);
+                    await Clients.Client(targetId.Trim()).SendAsync("ReceiveMessage", this.Context.ConnectionId, message);
+                }
 
-                await Clients.All.SendAsync("ReceiveMessage", this.Context.User.Identity.Name, message);
             }
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            //await Clients.All.SendAsync("ReceiveMessage", "SYSTEM", $"{this.Context.User.Identity.Name}님이 입장하였습니다.");
+            await Clients.All.SendAsync("ReceiveMessage", "SYSTEM", $"{this.Context.ConnectionId}님이 입장하였습니다.");
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            //await Clients.All.SendAsync("ReceiveMessage", "SYSTEM", $"{this.Context.User.Identity.Name}님이 퇴장하였습니다.");
+            await Clients.All.SendAsync("ReceiveMessage", "SYSTEM", $"{this.Context.ConnectionId}님이 퇴장하였습니다.");
+
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
